2ae3550a235ea2a2ca3f7d8530395ed6f3de4b1f
Steffen Pable <past1011(at)HS-Karlsruhe.de>
2013-05-07 13:23:03 +0200

    + service klassen von artikel

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -1,14 +1,29 @@
 package de.shop.artikelverwaltung.domain;
 
+import static de.shop.util.Constants.MIN_ID;
+
 import java.io.Serializable;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import de.shop.util.IdGroup;
 
 public class Artikel implements Serializable {
 	private static final long serialVersionUID = 1472129607838538329L;
 	
+	public static final int BEZEICHNUNG_LENGTH_MIN = 2;
+	public static final int BEZEICHNUNG_LENGTH_MAX = 32;
+	
 	// TODO Bean Validation
+	@Min(value = MIN_ID, message = "{artikelverwaltung.artikel.id.min}", groups = IdGroup.class)
 	private Long id;
 	
 	// TODO Bean Validation
+	@NotNull(message = "{artikelverwaltung.artikel.bezeichnung.notNull}")
+	@Size(min = BEZEICHNUNG_LENGTH_MIN, max = BEZEICHNUNG_LENGTH_MAX,
+	      message = "{artikelverwaltung.artikel.bezeichnung.length}")
 	private String bezeichnung;
 	
 	public Long getId() {
