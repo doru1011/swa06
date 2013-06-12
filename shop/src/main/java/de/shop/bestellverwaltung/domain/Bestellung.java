f81b7fb2aab8061d2835d5e5ddb70e8a5c76835c
Steffen Pable <past1011(at)HS-Karlsruhe.de>
2013-06-12 19:00:45 +0200

    Update Bestellung in der Bestellverwaltung und Anpassung der Constants Klasse.

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -1,37 +1,134 @@
 package de.shop.bestellverwaltung.domain;
 
 import static de.shop.util.Constants.MIN_ID;
+import static de.shop.util.Constants.KEINE_ID;
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.CascadeType.REMOVE;
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.TemporalType.TIMESTAMP;
 
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.logging.Logger;
 
 import de.shop.kundenverwaltung.domain.AbstractKunde;
 import de.shop.util.IdGroup;
 import de.shop.util.PreExistingGroup;
 
-
+@Entity
+@Table(name = "bestellung")
+@NamedQueries({
+	@NamedQuery(name  = Bestellung.FIND_BESTELLUNGEN_BY_KUNDE,
+                query = "SELECT b"
+			            + " FROM   Bestellung b"
+						+ " WHERE  b.kunde = :" + Bestellung.PARAM_KUNDE),
+	@NamedQuery(name  = Bestellung.FIND_KUNDE_BY_ID,
+ 			    query = "SELECT b.kunde"
+                        + " FROM   Bestellung b"
+  			            + " WHERE  b.id = :" + Bestellung.PARAM_ID)
+})
 public class Bestellung implements Serializable {
 	private static final long serialVersionUID = 1618359234119003714L;
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
+	private static final String PREFIX = "Bestellung.";
+	public static final String FIND_BESTELLUNGEN_BY_KUNDE = PREFIX + "findBestellungenByKunde";
+	public static final String FIND_KUNDE_BY_ID = PREFIX + "findBestellungKundeById";
+	
+	public static final String PARAM_KUNDE = "kunde";
+	public static final String PARAM_ID = "id";
+	
+	@Id
+	@GeneratedValue
+	@Column(nullable = false, updatable = false)
 	@Min(value = MIN_ID, message = "{bestellverwaltung.bestellung.id.min}", groups = IdGroup.class)
-	private Long id;
+	private Long id = KEINE_ID;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "kunde_fk", nullable = false, insertable = false, updatable = false)
+	@NotNull(message = "{bestellverwaltung.bestellung.kunde.notNull}", groups = PreExistingGroup.class)
+	@JsonIgnore
+	private AbstractKunde kunde;	
+			
+	@Transient
+	private URI kundeUri;
 	
 	private boolean ausgeliefert;
 	
-	@NotNull(message = "{bestellverwaltung.bestellung.kunde.notNull}", groups = PreExistingGroup.class)
-	@JsonIgnore
-	private AbstractKunde kunde;
-	
-	private URI kundeUri;
-	
-	@NotNull(message = "{bestellverwaltung.bestellung.bestellpositionen.notNull}")
+	@OneToMany(fetch = EAGER, cascade = { PERSIST, REMOVE })
+	@JoinColumn(name = "bestellung_fk", nullable = false)
+	@OrderColumn(name = "idx", nullable = false)
+	@NotEmpty(message = "{bestellverwaltung.bestellung.bestellpositionen.notEmpty}")
+	@Valid
 	private List<Bestellposition> bestellpositionen;
+	
+	@Column(nullable = false)
+	@Temporal(TIMESTAMP)
+	@JsonIgnore
+	private Date erzeugt;
+	
+	@Column(nullable = false)
+	@Temporal(TIMESTAMP)
+	@JsonIgnore
+	private Date aktualisieren;
+	
+	@Column(nullable = false)
+	@Temporal(TIMESTAMP)
+	@JsonIgnore
+	private Date aktualisiert;
+	
+	public Bestellung(){
+		super();
+	}
+	
+	public Bestellung(List<Bestellposition> bestellpositionen) {
+		super();
+		this.bestellpositionen = bestellpositionen;
+	}
+	
+	@PrePersist
+	private void prePersist() {
+		erzeugt = new Date();
+		aktualisiert = new Date();
+	}
+	
+	@PostPersist
+	private void postPersist() {
+		LOGGER.debugf("Neue Bestellung mit ID=%d", id);
+	}
+	
+	@PreUpdate
+	private void preUpdate() {
+		aktualisiert = new Date();
+	}
 	
 	public Long getId() {
 		return id;
@@ -60,11 +157,22 @@ public class Bestellung implements Serializable {
 	}
 	
 	public List<Bestellposition> getBestellpositionen() {
-		return bestellpositionen;
+		if (bestellpositionen == null) {
+			return null;
+		}
+		return Collections.unmodifiableList(bestellpositionen);
 	}
 	
 	public void setBestellpositionen(List<Bestellposition> bestellpositionen) {
-		this.bestellpositionen = bestellpositionen;
+		if (this.bestellpositionen == null) {
+			this.bestellpositionen = bestellpositionen;
+			return;
+	}
+		// Wiederverwendung der vorhandenen Collection
+		this.bestellpositionen.clear();
+		if (bestellpositionen != null) {
+			this.bestellpositionen.addAll(bestellpositionen);
+		}
 	}
 	
 	@Override
@@ -100,3 +208,4 @@ public class Bestellung implements Serializable {
 	}
 	
 }
+
--- shop/src/main/java/de/shop/util/Constants.java
+++ shop/src/main/java/de/shop/util/Constants.java
@@ -1,6 +1,8 @@
 package de.shop.util;
 
 public final class Constants {
+	// JPA
+	public static final Long KEINE_ID = null;
 	public static final long MIN_ID = 1L;
 	public static final long MIN_PREIS = 0L;
 	
